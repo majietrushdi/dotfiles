147ca6409410bc3f5ef21d3a0d6868c24435c08b
Rushdi Majiet <rushdi.majiet@hellopeter.com>
2024-04-03 09:03:03 +0200

    ensure php files only use 4 spaces instead of 2

--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -2,3 +2,15 @@ local opt = vim.opt
 
 opt.relativenumber = true
 vim.o.wrap = false
+
+-- opt.shiftwidth = 4
+-- opt.tabstop = 4
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "php",
+  callback = function()
+    vim.opt.shiftwidth = 4
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+  end,
+})
